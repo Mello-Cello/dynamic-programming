c27c87488c3dd28d7fe1fa3082c80db274554e64
Elle Kosling <celloelle@gmail.com>
2019-10-12 14:12:50 -0700

    Added solution for my first approach.

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -8,12 +8,32 @@ def max_sub_array(nums)
     max_so_far = nums[0]
     sum_ending_here = nums[0]
 
+    # -----------------------------------------------------
+    # SOLUTION 1
     nums[1..-1].each do |num|
+
         sum_ending_here = [sum_ending_here + num, num].max
+        
         if max_so_far < sum_ending_here
             max_so_far = sum_ending_here
         end
     end
+
+    # -----------------------------------------------------
+   
+    # SOLUTION 2
+
+    # nums[1..-1].each do |num|
+        
+    #     sum_ending_here = sum_ending_here + num
+        
+    #     if max_so_far < sum_ending_here
+    #         max_so_far = sum_ending_here
+    #     elsif sum_ending_here < 0
+    #         sum_ending_here = 0
+    #     end
+    # end
+    # ----------------------------------------------------- 
     
     return max_so_far
 end
